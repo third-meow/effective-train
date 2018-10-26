7a97a57a5c9881885f88d55576c6375afe2b0de1
The third meow <jsheaandcats@gmail.com>
2018-10-26 22:34:25 +1300

    improve commenting, change to new pickle names

--- setup_data.py
+++ setup_data.py
@@ -4,41 +4,43 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import random
+import numpy as np
 
-#where the dataset is
+# where the dataset is
 DATA_DIRECTORY = '/home/third-meow/datasets/CMUfaces'
-#the categories of images
+# the categories of images
 EXPRESSIONS = ['sad', 'happy', 'angry', 'neutral']  
 
 train_data = []
 
-#for all images in all expressions
+# 
 for expression in EXPRESSIONS:
-    #create path to expression
+    # create path to expression
     expression_path = os.path.join(DATA_DIRECTORY, expression)  
 
     for img_file in os.listdir(expression_path):
-        #create path to image
+        # create path to image
         img_path = os.path.join(expression_path, img_file)
 
-        #load image with opencv
+        # load image with opencv
         img = cv2.resize(cv2.imread(img_path, cv2.IMREAD_GRAYSCALE), (100, 100))
 
-        #append image and label to train data
+        # convert expression into array
         train_data.append([img, expression])
 
 #shuffle list of image-label pairs
+# shuffle list of image-label pairs
 random.shuffle(train_data)
 
 
-#split train data into lists x and y for image and label
+# split train data into lists x and y for image and label
 x = []
 y = []
 for i in train_data:
     x.append(i[0])
     y.append(i[1])
 
-#pickle the x and y lists
-pickle.dump(x, open('xdata.p', 'wb'))
-pickle.dump(y, open('ydata.p', 'wb'))
+# pickle the x and y lists
+pickle.dump(x, open('saved/xtrain.p', 'wb'))
+pickle.dump(y, open('saved/ytrain.p', 'wb'))
 
