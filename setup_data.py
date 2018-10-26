498c47611c756faf3e5ca6983809e69ecf2ca4d2
The third meow <jsheaandcats@gmail.com>
2018-10-26 22:35:27 +1300

    downsize images, fix expression format

--- setup_data.py
+++ setup_data.py
@@ -23,12 +23,26 @@ for expression in EXPRESSIONS:
         img_path = os.path.join(expression_path, img_file)
 
         # load image with opencv
-        img = cv2.resize(cv2.imread(img_path, cv2.IMREAD_GRAYSCALE), (100, 100))
+        img = cv2.resize(cv2.imread(img_path, cv2.IMREAD_GRAYSCALE), (70, 70))
 
         # convert expression into array
-        train_data.append([img, expression])
+        expression_arr = np.zeros(4) 
+        if expression == 'angry':
+            expression_arr[0] = 1.0
 
-#shuffle list of image-label pairs
+        elif expression == 'happy':
+            expression_arr[1] = 1.0
+            
+        elif expression == 'neutral':
+            expression_arr[2] = 1.0
+
+        elif expression == 'sad':
+            expression_arr[3] = 1.0
+
+
+        # append image and label to train data
+        train_data.append([img, expression_arr])
+
 # shuffle list of image-label pairs
 random.shuffle(train_data)
 
